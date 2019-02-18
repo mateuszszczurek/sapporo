42ebadec9f1736a2f1703211178740ebadc07fba
Mateusz <mateusz.szczurek86@gmail.com>
2019-02-18 22:41:01 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/sapporo/precache-manifest.33b298b7c60291572b1eae8b0bb678ed.js"
+  "/sapporo/precache-manifest.9a15cdc8dbef043f7eda6b8247a10435.js"
 );
 
 workbox.clientsClaim();
